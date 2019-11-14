ddfb1f51cf628f43dd52266f88880d5ffbb983ff
NJA <39605984+nabbott335@users.noreply.github.com>
2019-11-14 10:19:20 -0500

    Update GitProject.cpp

--- GitProject.cpp
+++ GitProject.cpp
@@ -1,8 +1,8 @@
 /* GitProject.cpp : Defines the entry point for the console application.
 
-   
+
 */
-   
+
 //
 
 #include <iostream>
@@ -13,17 +13,18 @@ using  namespace std;
 //another change
 char * names[] = { "yxiao025","Xliu008", "MLalanne25","demoinclass", "mmail002", "rmarh001", "qle002",
 					"winkhanh", "Yosigi12944","tpeck003", "PerryW11", "johannsilvanus", "je1013", "tylerspags",
-					 "ClutchDutch", "mle003","dvalerio001", "jminc005",
+					 "ClutchDutch", "mle003","dvalerio001", "jminc005", "nabbott335"
+         }
 
 
 int main()
-{ 
+{
 
 	cout << "hello class" << endl;
 
 	string s;
 
-	
+
 	for ( int x = 0 ; names[ x ] != NULL ; x++ ) {
 		cout << names[x] << endl;
 	}
