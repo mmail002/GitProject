1e05692f97b55c9b8907ebc1c3addcaab2c6936e
MARTIN\Martin Mailloux <mmail002@plattsburgh.edu>
2020-03-30 22:41:39 -0400

    a commit message

--- GitProject.cpp
+++ GitProject.cpp
@@ -11,7 +11,7 @@ using  namespace std;
 
 //please add your github user to names
 //another change
-char * names[] = { "myname", "yourname"
+char * names[] = { "myname", "yourname","atest"
          }
 
 
