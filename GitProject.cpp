979337fece492f0aee458685b1ed16d82935dc54
MARTIN\Martin Mailloux <mmail002@plattsburgh.edu>
2020-03-30 22:39:37 -0400

    reset of list of users

--- GitProject.cpp
+++ GitProject.cpp
@@ -11,9 +11,7 @@ using  namespace std;
 
 //please add your github user to names
 //another change
-char * names[] = { "yxiao025","Xliu008", "MLalanne25","demoinclass", "mmail002", "rmarh001", "qle002",
-					"winkhanh", "Yosigi12944","tpeck003", "PerryW11", "johannsilvanus", "je1013", "tylerspags",
-					 "ClutchDutch", "mle003","dvalerio001", "jminc005", "nabbott335","jakejasminjk"
+char * names[] = { "myname"
          }
 
 
