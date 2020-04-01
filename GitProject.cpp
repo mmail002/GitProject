06a52f2eb34ef2ab6ccc87ee40b20cd7a10a9b31
ovil101 <rlioy1211@gmail.com>
2020-04-01 14:29:22 -0700

    first commit

--- GitProject.cpp
+++ GitProject.cpp
@@ -1,30 +1,20 @@
 /* GitProject.cpp : Defines the entry point for the console application.
-
-
 */
-
-//
-
 #include <iostream>
 
 using  namespace std;
 
 //please add your github user to names
 //another change
-char * names[] = { "myname", "aaajdoe@plattsburgh.edu"
-         };
-
+char * names[] = { "ryan lioy", "rlioy001@plattsburgh.edu"};
 
 int main()
 {
-
-	cout << "hello class" << endl;
+	cout << "hello class" << endl; // this is a comment
 
 	string s;
-
 
 	for ( int x = 0 ; names[ x ] != NULL ; x++ ) {
 		cout << names[x] << endl;
 	}
-
 }
