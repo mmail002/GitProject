63d2d77f6deefd70658ff114f5cabd8a9e13e9ef
mmeng004 <73732407+mmeng004@users.noreply.github.com>
2020-11-02 14:38:59 -0500

    Fixed/Updated

--- GitProject.cpp
+++ GitProject.cpp
@@ -8,7 +8,7 @@ using  namespace std;
 
 //please add your github user to names
 //another change
-char * names[] = { "mmail002", "Naol Legesse","Nlege001 ", "Meron Mengistu", "mmeng004"};
+char * names[] = { "mmail002", "johndoe!!!!!", "PurpleVS", "MattThung2400", "ELARA004","NelsonTejeda" "Naol Legesse","Nlege001 ", "mmeng004" "Steven Colletti","Scolletti98","rmoor008"};
 
 int main()
 {
