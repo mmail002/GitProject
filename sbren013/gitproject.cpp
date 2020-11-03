2d8fa09d56e88f413d7d7dd7f2b598ae64a8b5fc
sbren013 <70453910+sbren013@users.noreply.github.com>
2020-11-02 23:48:45 -0500

    Create gitproject.cpp

---
+++ sbren013/gitproject.cpp
@@ -0,0 +1,22 @@
+/*
+Please add your name to names
+*/
+
+#include <iostream>
+
+using  namespace std;
+
+//please add your github user to names
+//another change
+char * names[] = { "mmail002", "johndoe!!!!!", "PurpleVS", "MattThung2400", "ELARA004","NelsonTejeda" "Naol Legesse","Nlege001 ", "mmeng004" "Steven Colletti","Scolletti98","rmoor008","whoen001", "draushi99", "sbren013"};
+
+int main()
+{
+	cout << "hello class" << endl; // this is a comment
+
+	string s;
+
+	for ( int x = 0 ; names[ x ] != NULL ; x++ ) {
+		cout << names[x] << endl;
+	} 
+}
