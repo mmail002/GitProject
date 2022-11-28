ad4e3b85ec997edc41352c55a0ade2e8b8f979c3
dgrant6 <dgran013@plattsburgh.edu>
2022-11-27 21:49:10 -0500

    dgrant6

--- GitProject.cpp
+++ GitProject.cpp
@@ -9,7 +9,7 @@ using  namespace std;
 
 //please add your github user to names
 //another change   
-char * names[] = { "add your name to this list", "mmail002", "nortondav18", "Mooshimi", "abrun014", "Aouncute", "shanthan-reddy372" };
+char * names[] = { "add your name to this list", "mmail002", "nortondav18", "Mooshimi", "abrun014", "Aouncute", "shanthan-reddy372", "dgrant6" };
 
 int main()
 {
